ef200d62dd3c6013806a84ed643f362cee5b8aaa
SN-20061230 <sardorbek2006.normurodov@gmail.com>
2023-11-24 21:47:11 +0500

    Madrid

--- app/src/main/java/com/example/myapplication/DetailScreen.kt
+++ app/src/main/java/com/example/myapplication/DetailScreen.kt
@@ -1,20 +1,17 @@
 package com.example.myapplication
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
